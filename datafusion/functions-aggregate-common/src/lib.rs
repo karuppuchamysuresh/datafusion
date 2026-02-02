2a0862ded377f8d2e1f2c6eea553aa299deb91c8
Sureshkumar Karuppuchamy <skaruppuchamy05@gmail.com>
2026-02-01 23:47:16 -0800

    Enable clone_on_ref_ptr Clippy lint at workspace level
    Move clone_on_ref_ptr lint configuration from individual files to
    workspace level in root Cargo.toml. This centralizes lint configuration
    and ensures consistent enforcement across all crates.
    
    Changes:
    - Add clone_on_ref_ptr = "deny" to [workspace.lints.clippy] section
    - Remove #![deny(clippy::clone_on_ref_ptr)] from 19 individual files
    - All subcrates inherit workspace lints through [lints] workspace = true
    
    Closes #17083

--- datafusion/functions-aggregate-common/src/lib.rs
+++ datafusion/functions-aggregate-common/src/lib.rs
@@ -29,7 +29,6 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 // Make sure fast / cheap clones on Arc are explicit:
 // https://github.com/apache/datafusion/issues/11143
-#![deny(clippy::clone_on_ref_ptr)]
 #![cfg_attr(test, allow(clippy::needless_pass_by_value))]
 
 pub mod accumulator;
